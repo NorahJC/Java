af5d1aac18f571f0d2cba19087a59bfac6abde92
NorahJC <norahjeanc@gmail.com>
2020-10-30 23:54:03 -0400

    word splitting

---
+++ WordCount.java
@@ -0,0 +1,7 @@
+class WordCount{
+    public static void main(String args[]) {
+    String sentence = "The cow jumped over the moon";
+    String[] words = sentence.split("\\s+");
+    System.out.println(words);
+    }
+}
